0d53c6bb05e2720f1b31cf7b7767ba042d202b27
SysoievB <sysoiev.bogdan.ua@gmail.com>
2025-06-08 14:16:26 +0200

    Init commit

---
+++ src/main/java/com/nio_webflux_crud/NioWebfluxCrudApplication.java
@@ -0,0 +1,13 @@
+package com.nio_webflux_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NioWebfluxCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NioWebfluxCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nio_webflux_crud/NioWebfluxCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nio_webflux_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NioWebfluxCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
